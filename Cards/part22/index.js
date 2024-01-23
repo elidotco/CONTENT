2903678720d32d4bd1769e1625120f52699cca6e
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-01-23 14:42:52 +0000

    cards part 22

---
+++ Cards/part22/index.js
@@ -0,0 +1,9 @@
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
